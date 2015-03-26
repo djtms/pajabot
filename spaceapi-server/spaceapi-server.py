75e7c9818af792ff187f93e25f8f15faee5fddb9
System User <cos@dalek.5w.fi>
2015-03-26 21:39:48 +0200

    Run updater script

--- spaceapi-server/spaceapi-server.py
+++ spaceapi-server/spaceapi-server.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
 import zerorpc
+import os
 
 class SpaceAPI(object):
     def updateStatus(self, labOpen, topic):
 	print "Set lab open " + str(labOpen) + " and topic " + topic
+	os.system("~/updatestatus.sh " + str(labOpen) + " Foo")
         return "Update ok"
 
 s = zerorpc.Server(SpaceAPI())
